ca0e3694999f608120ac4a7c1d4189621c0c02d6
s-araqayyum <saraqayyum268@gmail.com>
2022-10-16 00:42:16 +0500

    product inventory

--- backend/controllers/product.js
+++ backend/controllers/product.js
@@ -1,4 +1,24 @@
+import product from "../models/product.js"
 
-export const getProduct = (req,res) => {
-    res.send('This works!')
+export const getProduct = async (req,res) => {
+    try {
+        const products = await product.find()
+        console.log(products)
+        res.status(200).json(products)
+    } catch (error) {
+        res.status(404).json({message: error.message})
+    }
+}
+
+export const createProduct = async (req,res) => {
+    const prod = req.body
+    const newProduct = new product(prod)
+
+    try {
+        await newProduct.save()
+        res.status(201).json(newProduct)
+
+    } catch (error) {
+        res.status(409).json({message: error.message})
+    }
 }
--- backend/routes/product.js
+++ backend/routes/product.js
@@ -1,7 +1,9 @@
 import express from 'express'
-import { getProduct } from '../controllers/product.js';
+import { getProduct, createProduct } from '../controllers/product.js';
 const router = express.Router();
 
 router.get('/', getProduct);
+router.post('/', createProduct);
+
 
 export default router
