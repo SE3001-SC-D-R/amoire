d52c0965933bdabd3bc037f36da10cf06af0f39a
s-araqayyum <saraqayyum268@gmail.com>
2022-10-20 21:46:37 +0500

    product inventory

--- frontend/src/Pages/ProductCatalog.js
+++ frontend/src/Pages/ProductCatalog.js
@@ -3,10 +3,10 @@ import React from 'react'
 export default function ProductCatalog() {
   return (
     <React.Fragment>
-		<nav class="custom-navbar navbar navbar navbar-expand-md navbar-dark bg-dark" arial-label="Furni navigation bar">
+		<nav class="custom-navbar navbar navbar navbar-expand-md navbar-dark bg-dark">
 			<div class="container">
-				<a class="navbar-brand" href="index.html">Furni<span>.</span></a>
-				<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsFurni" aria-controls="navbarsFurni" aria-expanded="false" aria-label="Toggle navigation">
+				<a class="navbar-brand" href="index.html">armoire<span>.</span></a>
+				<button class="navbar-toggler" type="button" data-bs-toggle="collapse"  aria-label="Toggle navigation">
 					<span class="navbar-toggler-icon"></span>
 				</button>
 				<div class="collapse navbar-collapse" id="navbarsFurni">
@@ -14,36 +14,19 @@ export default function ProductCatalog() {
 						<li class="nav-item ">
 							<a class="nav-link" href="index.html">Home</a>
 						</li>
-						<li class="active"><a class="nav-link" href="shop.html">Shop</a></li>
-						<li><a class="nav-link" href="about.html">About us</a></li>
-						<li><a class="nav-link" href="services.html">Services</a></li>
-						<li><a class="nav-link" href="blog.html">Blog</a></li>
-						<li><a class="nav-link" href="contact.html">Contact us</a></li>
+						<li class="active"><a class="nav-link">Product Catalogue</a></li>
+						<li><a class="nav-link">Orders</a></li>
+						<li><a class="nav-link">Services</a></li>
+						<li><a class="nav-link">History</a></li>
+						<li><a class="nav-link">Contact us</a></li>
 					</ul>
 
 					<ul class="custom-navbar-cta navbar-nav mb-2 mb-md-0 ms-5">
 						<li><a class="nav-link" href="#"><img src="Assets/images/user.svg"/></a></li>
-						<li><a class="nav-link" href="cart.html"><img src="Assets/images/cart.svg"/></a></li>
 					</ul>
 				</div>
 			</div>		
 		</nav>
-			<div class="hero">
-				<div class="container">
-					<div class="row justify-content-between">
-						<div class="col-lg-5">
-							<div class="intro-excerpt">
-								<h1>Shop</h1>
-							</div>
-						</div>
-						<div class="col-lg-7">
-							
-						</div>
-					</div>
-				</div>
-			</div>
-
-		
 
 		<div class="untree_co-section product-section before-footer-section">
 		    <div class="container">
@@ -55,7 +38,11 @@ export default function ProductCatalog() {
 							<strong class="product-price">$50.00</strong>
 
 							<span class="icon-cross">
-								<img src="Assets/images/cross.svg" class="img-fluid"/>
+								<img src="Assets/images/update.png" class="img-fluid"/>
+							</span>
+
+                            <span class="icon-del">
+								<img src="Assets/images/del.png" class="img-fluid"/>
 							</span>
 						</a>
 					</div> 
@@ -67,7 +54,11 @@ export default function ProductCatalog() {
 							<strong class="product-price">$50.00</strong>
 
 							<span class="icon-cross">
-								<img src="Assets/images/cross.svg" class="img-fluid"/>
+								<img src="Assets/images/update.png" class="img-fluid"/>
+							</span>
+
+                            <span class="icon-del">
+								<img src="Assets/images/del.png" class="img-fluid"/>
 							</span>
 						</a>
 					</div> 
@@ -79,7 +70,11 @@ export default function ProductCatalog() {
 							<strong class="product-price">$78.00</strong>
 
 							<span class="icon-cross">
-								<img src="Assets/images/cross.svg" class="img-fluid"/>
+								<img src="Assets/images/update.png" class="img-fluid"/>
+							</span>
+
+                            <span class="icon-del">
+								<img src="Assets/images/del.png" class="img-fluid"/>
 							</span>
 						</a>
 					</div>
@@ -91,7 +86,11 @@ export default function ProductCatalog() {
 							<strong class="product-price">$43.00</strong>
 
 							<span class="icon-cross">
-								<img src="Assets/images/cross.svg" class="img-fluid"/>
+								<img src="Assets/images/update.png" class="img-fluid"/>
+							</span>
+
+                            <span class="icon-del">
+								<img src="Assets/images/del.png" class="img-fluid"/>
 							</span>
 						</a>
 					</div>
@@ -103,7 +102,11 @@ export default function ProductCatalog() {
 							<strong class="product-price">$50.00</strong>
 
 							<span class="icon-cross">
-								<img src="Assets/images/cross.svg" class="img-fluid"/>
+								<img src="Assets/images/update.png" class="img-fluid"/>
+							</span>
+
+                            <span class="icon-del">
+								<img src="Assets/images/del.png" class="img-fluid"/>
 							</span>
 						</a>
 					</div> 
@@ -115,7 +118,11 @@ export default function ProductCatalog() {
 							<strong class="product-price">$50.00</strong>
 
 							<span class="icon-cross">
-								<img src="Assets/images/cross.svg" class="img-fluid"/>
+								<img src="Assets/images/update.png" class="img-fluid"/>
+							</span>
+
+                            <span class="icon-del">
+								<img src="Assets/images/del.png" class="img-fluid"/>
 							</span>
 						</a>
 					</div> 
@@ -127,7 +134,11 @@ export default function ProductCatalog() {
 							<strong class="product-price">$78.00</strong>
 
 							<span class="icon-cross">
-								<img src="Assets/images/cross.svg" class="img-fluid"/>
+								<img src="Assets/images/update.png" class="img-fluid"/>
+							</span>
+
+                            <span class="icon-del">
+								<img src="Assets/images/del.png" class="img-fluid"/>
 							</span>
 						</a>
 					</div>
@@ -139,7 +150,11 @@ export default function ProductCatalog() {
 							<strong class="product-price">$43.00</strong>
 
 							<span class="icon-cross">
-								<img src="Assets/images/cross.svg" class="img-fluid"/>
+								<img src="Assets/images/update.png" class="img-fluid"/>
+							</span>
+
+                            <span class="icon-del">
+								<img src="Assets/images/del.png" class="img-fluid"/>
 							</span>
 						</a>
 					</div>
